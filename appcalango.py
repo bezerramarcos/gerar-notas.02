0d2843feb4a2742349996c73f4ccc1acd619cafd
Marcos Aurélio <82787088+bezerramarcos@users.noreply.github.com>
2024-10-15 20:37:23 -0300

    Add files via upload

---
+++ appcalango.py
@@ -0,0 +1,59 @@
+#calculo de alunos de uma escola
+
+
+nome=input("digite o nome do aluno  \n")
+escola=input('digite o nome da escola \n')
+qtde_alunos=int(input('digite a quantidade de alunoo \n'))
+mediaNota=float(input('digite a media  \n '))
+print(nome + " estuda na escola " + escola )
+print("possui: " , qtde_alunos, " alunos  ")
+print("a media da escola e de : " + str(mediaNota))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+
+      
+    
+    
+     
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+
+
